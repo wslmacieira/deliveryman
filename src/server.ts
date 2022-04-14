2c534788dfa2be0ffc6a45d754a84d27c335e8e2
wslmacieira <wslmacieira@outlook.com>
2022-04-14 00:11:01 -0300

    criando server com express

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get("/", (request, response) => {
+  response.json({
+    message: "Hello world"
+  })
+})
+
+
+app.listen(3000, () => console.log("Server is running"))
